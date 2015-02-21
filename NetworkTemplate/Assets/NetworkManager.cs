79c6be69f6dcc036eef46662604d18947a888be1
Matt Church <churchmf@gmail.com>
2015-02-21 09:54:32 -0700

    Basic hosting, connecting, player assignment and movement logic

---
+++ NetworkTemplate/Assets/NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+
+//http://createathingunity.blogspot.co.uk/
+/// <summary>
+/// Network manager handles initialization of players connecting to a server
+/// </summary>
+public class NetworkManager : MonoBehaviour
+{
+		public Transform playerPrefab;
+
+		void OnServerInitialized ()
+		{ 
+				if (Network.isServer) {
+					MakePlayer (Network.player);
+				}
+		}
+	
+		void OnConnectedToServer ()
+		{
+			networkView.RPC ("MakePlayer", RPCMode.Server, Network.player);
+		}
+
+		[RPC]
+		void MakePlayer (NetworkPlayer thisPlayer)
+		{
+				Transform newPlayer = Network.Instantiate (playerPrefab, playerPrefab.position, playerPrefab.rotation, 0) as Transform;
+				
+				if (Network.isServer) {
+						var rigidBody = newPlayer.gameObject.AddComponent<Rigidbody> ();
+						rigidBody.freezeRotation = true;
+				}
+
+				if (thisPlayer != Network.player) {
+						networkView.RPC ("EnableCamera", thisPlayer, newPlayer.networkView.viewID);
+				} else {
+						EnableCamera (newPlayer.networkView.viewID);
+				}
+		}
+	
+		[RPC]
+		void EnableCamera (NetworkViewID viewID)
+		{
+				GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+				foreach (GameObject playerObject in players) {
+							if (playerObject.networkView.viewID == viewID) {
+								playerObject.GetComponent<NetworkMovement> ().haveControl = true;
+								Transform myCamera = playerObject.transform.Find ("Camera");
+								myCamera.camera.enabled = true;
+								myCamera.camera.GetComponent<AudioListener> ().enabled = true;
+								break;
+						}
+				}
+		}
+}
---
+++ NetworkTemplate/Assets/ServerManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Server manager handles the hosting and connecting to the game server. Provides a basic GUI for starting, finding, and connecting to games.
+/// </summary>
+public class ServerManager : MonoBehaviour {
+	string registeredName = "somekindofuniquename";
+	float refreshRequestLength = 3.0f;
+	HostData[] hostData;
+	public string chosenGameName = "";
+
+	private void StartServer ()
+	{
+		Network.InitializeServer (16, Random.Range (2000, 2500), !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (registeredName, chosenGameName);
+	}
+	
+	private IEnumerator RefreshHostList ()
+	{
+		MasterServer.RequestHostList (registeredName);
+		float timeEnd = Time.time + refreshRequestLength;
+		while (Time.time < timeEnd) {
+			hostData = MasterServer.PollHostList ();
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
+	public void OnGUI ()
+	{
+		if (Network.isClient || Network.isServer) {
+			return;
+		}
+		if (chosenGameName == "") {
+			GUI.Label (new Rect (Screen.width / 2 - Screen.width / 10, Screen.height / 2 - Screen.height / 20, Screen.width / 5, Screen.height / 20), "Game Name");
+		}
+		chosenGameName = GUI.TextField (new Rect (Screen.width / 2 - Screen.width / 10, Screen.height / 2 - Screen.height / 20, Screen.width / 5, Screen.height / 20), chosenGameName, 25);
+		if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 10, Screen.height / 2, Screen.width / 5, Screen.height / 10), "Start New Server")) {
+			StartServer ();
+		}
+		if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 10, Screen.height / 2 + Screen.height / 10, Screen.width / 5, Screen.height / 10), "Find Servers")) {
+			StartCoroutine (RefreshHostList ());
+		}
+		if (hostData != null) {
+			for (int i = 0; i < hostData.Length; i++) {
+				if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 10, Screen.height / 2 + ((Screen.height / 20) * (i + 4)), Screen.width / 5, Screen.height / 20), hostData [i].gameName)) {
+					Network.Connect (hostData [i]);
+				}
+			}
+		}
+	}
+}
